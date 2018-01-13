cd5a3437d76e876870d7dc3e767d0776eaa0eeaa
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-01-13 19:17:44 +0100

    added new gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,51 @@
+// Commons
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Sass
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// JS
+const concat = require('gulp-concat');
+
+// Images
+const imagemin = require('gulp-imagemin');
+
+gulp.task('default', ['sass', 'javascript', 'imagemin']);
 
 gulp.task('sass', () => {
     return gulp.src('./app/sass/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('watch', () => {
     gulp.watch('./app/sass/*.scss', ['sass']);
+});
+
+gulp.task('javascript', () => {
+    gulp.src('./app/js/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('custom.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('imagemin', () => {
+    var imgSrc = 'src/images/*.+(png|jpg|gif)',
+        imgDst = 'build/images';
+
+    gulp.src('./app/images/*.+(png|jpg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('default',['imagemin'],function(){
 });
