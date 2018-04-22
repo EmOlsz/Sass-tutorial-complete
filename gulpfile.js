5c3106f9075ee659aa38c7c280f3b22ca1a5b17f
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2018-04-22 22:00:35 +0200

    updated gulpfile and gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,13 @@
-// Commons
 const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-
-// Sass
-const sass = require('gulp-sass');
+const sass= require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-
-// JS
-const concat = require('gulp-concat');
-
-// Images
+const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['sass', 'javascript', 'imagemin']);
-
 gulp.task('sass', () => {
-    return gulp.src('./app/sass/style.scss')
+    return gulp.src('./app/sass/main.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'nested'}))
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
@@ -27,25 +17,12 @@ gulp.task('sass', () => {
 });
 
 gulp.task('watch', () => {
-    gulp.watch('./app/sass/*/*.scss', ['sass']);
-});
-
-gulp.task('javascript', () => {
-    gulp.src('./app/js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(concat('custom.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./dist/js'))
+    gulp.watch('./app/sass/*/*.scss', ['sass'])
 });
 
 gulp.task('imagemin', () => {
-    var imgSrc = 'src/images/*.+(png|jpg|gif)',
-        imgDst = 'build/images';
-
     gulp.src('./app/images/*.+(png|jpg)')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/images'))
 });
 
-gulp.task('default',['imagemin'],function(){
-});
